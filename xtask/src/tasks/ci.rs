28bc6de737b00ad23feee90b4b5fd7b316eb7de9
damccull <401104+damccull@users.noreply.github.com>
2022-05-23 17:13:43 -0400

    Update `ci` xtask to output colors.

--- xtask/src/tasks/ci.rs
+++ xtask/src/tasks/ci.rs
@@ -1,37 +1,66 @@
-use std::process::Command;
+use colored::Colorize;
+use std::process::{Command, ExitStatus, Stdio};
 
 use crate::DynError;
 
 pub fn ci() -> Result<(), DynError> {
     println!("Running `cargo check`...");
-    let check = Command::new("cargo").args(["check"]).output()?;
-    println!("{}", std::str::from_utf8(&check.stdout)?);
-    println!("{}", std::str::from_utf8(&check.stderr)?);
+    let cargo_check_status = Command::new("cargo")
+        .args(["check", "-p", "ravendb-client"])
+        .stdout(Stdio::inherit())
+        .status()?;
 
     println!("Running `cargo clippy`...");
-    let clippy = Command::new("cargo").args(["clippy"]).output()?;
-    println!("{}", std::str::from_utf8(&clippy.stdout)?);
-    println!("{}", std::str::from_utf8(&clippy.stderr)?);
+    let cargo_clippy_status = Command::new("cargo")
+        .args(["clippy", "-p", "ravendb-client"])
+        .stdout(Stdio::inherit())
+        .status()?;
 
     println!("Running `cargo build`...");
-    let build = Command::new("cargo").args(["build"]).output()?;
-    println!("{}", std::str::from_utf8(&build.stdout)?);
-    println!("{}", std::str::from_utf8(&build.stderr)?);
+    let cargo_build_status = Command::new("cargo")
+        .args(["build", "-p", "ravendb-client"])
+        .stdout(Stdio::inherit())
+        .status()?;
 
     println!("Running `cargo test`...");
-    let test = Command::new("cargo").args(["test"]).output()?;
-    println!("{}", std::str::from_utf8(&test.stdout)?);
-    println!("{}", std::str::from_utf8(&test.stderr)?);
+    let cargo_test_status = Command::new("cargo")
+        .args(["test", "-p", "ravendb-client"])
+        .stdout(Stdio::inherit())
+        .status()?;
 
     println!("Running `cargo audit`...");
-    let audit = Command::new("cargo").args(["audit"]).output()?;
-    println!("{}", std::str::from_utf8(&audit.stdout)?);
-    println!("{}", std::str::from_utf8(&audit.stderr)?);
+    let cargo_audit_status = Command::new("cargo")
+        .args(["audit"])
+        .stdout(Stdio::inherit())
+        .status()?;
 
     println!("Running `cargo fmt`...");
-    let fmt = Command::new("cargo").args(["fmt"]).output()?;
-    println!("{}", std::str::from_utf8(&fmt.stdout)?);
-    println!("{}", std::str::from_utf8(&fmt.stderr)?);
+    let cargo_fmt_status = Command::new("cargo")
+        .args(["fmt"])
+        .stdout(Stdio::inherit())
+        .status()?;
+
+    print_error_with_status_code("cargo check", cargo_check_status);
+    print_error_with_status_code("cargo clippy", cargo_clippy_status);
+    print_error_with_status_code("cargo build", cargo_build_status);
+    print_error_with_status_code("cargo test", cargo_test_status);
+    print_error_with_status_code("cargo audit", cargo_audit_status);
+    print_error_with_status_code("cargo fmt", cargo_fmt_status);
 
     Ok(())
+}
+
+fn print_error_with_status_code(task: &str, status: ExitStatus) {
+    let code = match status.code() {
+        Some(x) => x.to_string(),
+        None => "<< no status code >>".to_string(),
+    };
+    if !status.success() {
+        println!(
+            "{} `{}` finished with a non-zero status code: {}",
+            "Error:".to_string().red(),
+            task.blue(),
+            code
+        );
+    }
 }
