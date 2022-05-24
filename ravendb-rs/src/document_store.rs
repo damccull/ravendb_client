83f7dcc2c7c9bc0193e8f6a9a2b386e14cd39bb1
damccull <401104+damccull@users.noreply.github.com>
2022-05-23 21:46:09 -0400

    Add in some state for the actor and a default impl

--- ravendb-rs/src/document_store.rs
+++ ravendb-rs/src/document_store.rs
@@ -1,5 +1,7 @@
 use tokio::sync::{broadcast, mpsc, oneshot};
 
+use crate::events::{ConversionEvents, CrudEvents, RequestEvents, SessionEvents};
+
 /// This a handle to the actor
 #[derive(Clone)]
 pub struct DocumentStore {
@@ -24,10 +26,36 @@ impl Default for DocumentStore {
 
 struct DocumentStoreActor {
     receiver: mpsc::Receiver<DocumentStoreMessage>,
+
+    conversion_events_sender: broadcast::Sender<ConversionEvents>,
+    crud_events_sender: broadcast::Sender<CrudEvents>,
+    request_events_sender: broadcast::Sender<RequestEvents>,
+    session_events_sender: broadcast::Sender<SessionEvents>,
+
+    certificate: Option<CertificatePlaceholder>,
+    conventions: Option<Conventions>,
+    database: Option<String>,
+    trust_store: Option<CertificatePlaceholder>,
+    urls: Vec<String>,
 }
 impl DocumentStoreActor {
     fn new(receiver: mpsc::Receiver<DocumentStoreMessage>) -> Self {
-        Self { receiver }
+        let (crud_sender, _) = broadcast::channel(100);
+        let (request_sender, _) = broadcast::channel(100);
+        let (conversion_sender, _) = broadcast::channel(100);
+        let (session_sender, _) = broadcast::channel(100);
+        Self {
+            receiver,
+            crud_events_sender: crud_sender,
+            request_events_sender: request_sender,
+            conversion_events_sender: conversion_sender,
+            session_events_sender: session_sender,
+            urls: Default::default(),
+            conventions: Default::default(),
+            database: Default::default(),
+            certificate: Default::default(),
+            trust_store: Some(CertificatePlaceholder),
+        }
     }
 
     fn handle_message(&mut self, msg: DocumentStoreMessage) {
@@ -38,13 +66,20 @@ impl DocumentStoreActor {
     }
 }
 
-enum DocumentStoreMessage {
-    Initialize,
-    Close,
-}
-
 async fn run_document_store_actor(mut actor: DocumentStoreActor) {
     while let Some(msg) = actor.receiver.recv().await {
         actor.handle_message(msg);
     }
 }
+
+enum DocumentStoreMessage {
+    Initialize,
+    Close,
+}
+
+// Placeholders below
+pub struct Conventions;
+pub struct CertificatePlaceholder;
+
+pub struct DatabaseChanges;
+pub struct DatabaseChangesBuilder;
