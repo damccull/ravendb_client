1d583e830180ac6c9fde97d6ac0b118fb31cbcb3
damccull <401104+damccull@users.noreply.github.com>
2022-06-30 16:02:26 -0600

    Add anyhow

--- raven-test/src/main.rs
+++ raven-test/src/main.rs
@@ -1,11 +1,12 @@
 use ravendb_client::DocumentStoreBuilder;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let urls = ["https://a.free.damccull.ravendb.cloud"];
     let _client = DocumentStoreBuilder::new()
         .set_client_certificate("free.damccull.client.certificate.pem")
         .set_urls(&urls)
-        .build();
+        .build()?;
     dbg!(_client);
+    Ok(())
 }
