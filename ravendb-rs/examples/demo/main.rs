856e6b772086388aa4a3cc7f47df201e611c1cc4
damccull <401104+damccull@users.noreply.github.com>
2022-09-16 23:01:59 -0400

    Move test project into examples.

--- ravendb-rs/examples/demo/main.rs
+++ ravendb-rs/examples/demo/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello World");
+use ravendb_client::DocumentStoreBuilder;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let urls = ["https://a.free.damccull.ravendb.cloud"];
+    let client = DocumentStoreBuilder::new()
+        .set_client_certificate("free.damccull.client.certificate.pem")
+        .require_https()
+        .set_urls(&urls)
+        .build()?;
+    dbg!(&client);
+    let session = client.open_session().await?;
+    dbg!(&session);
+    Ok(())
 }
