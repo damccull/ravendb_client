755856944748c394f180221c0adc8a0069b1191f
damccull <401104+damccull@users.noreply.github.com>
2022-09-17 00:46:22 -0400

    Set up tracing for the program

--- ravendb-rs/examples/demo/main.rs
+++ ravendb-rs/examples/demo/main.rs
@@ -1,7 +1,13 @@
 use ravendb_client::DocumentStoreBuilder;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    setup_tracing();
+
     let urls = ["https://a.free.damccull.ravendb.cloud"];
     let client = DocumentStoreBuilder::new()
         .set_client_certificate("free.damccull.client.certificate.pem")
@@ -13,4 +19,17 @@ async fn main() -> anyhow::Result<()> {
     let rdb_version = session.get_ravendb_version().await?;
     dbg!(&rdb_version);
     Ok(())
+}
+
+fn setup_tracing() {
+    // Redirect all `log`'s events to the subscriber
+    LogTracer::init().expect("Failed to set logger");
+    // Set up tracing
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("ravendb-rs-demo".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
 }
