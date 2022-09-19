ebd5bb2ce59bb4925f670f431c8f6f2fdb0c411d
damccull <401104+damccull@users.noreply.github.com>
2022-09-19 11:53:03 -0400

    Rename package and containing folder

--- ravendb_client/examples/demo/main.rs
+++ ravendb_client/examples/demo/main.rs
@@ -1,8 +1,9 @@
 use ravendb_client::DocumentStoreBuilder;
 use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+// use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_tree::HierarchicalLayer;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -31,10 +32,11 @@ fn setup_tracing() {
     LogTracer::init().expect("Failed to set logger");
     // Set up tracing
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("ravendb-rs-demo".into(), std::io::stdout);
+    //let formatting_layer = BunyanFormattingLayer::new("ravendb-rs-demo".into(), std::io::stdout);
     let subscriber = Registry::default()
         .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
+        //.with(JsonStorageLayer)
+        //.with(formatting_layer)
+        .with(HierarchicalLayer::new(2));
     set_global_default(subscriber).expect("Failed to set subscriber");
 }
