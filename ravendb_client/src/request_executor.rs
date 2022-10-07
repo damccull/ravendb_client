8528581576ec39ff7e297a76e5613abeea0bb755
damccull <401104+damccull@users.noreply.github.com>
2022-10-07 15:40:06 -0400

    Add variant to trigger an initial topology update

--- ravendb_client/src/request_executor.rs
+++ ravendb_client/src/request_executor.rs
@@ -5,11 +5,15 @@ mod request_executor_handle;
 pub use request_executor_actor::RequestExecutorActor;
 pub use request_executor_error::RequestExecutorError;
 pub use request_executor_handle::RequestExecutor;
+use reqwest::Url;
 use tokio::sync::oneshot;
 
 pub(crate) enum RequestExecutorMessage {
     ExecuteRequest {
         respond_to: oneshot::Sender<Result<(), RequestExecutorError>>,
         request: reqwest::Request,
+    },
+    FirstTopologyUpdate {
+        initial_urls: Vec<Url>,
     },
 }
