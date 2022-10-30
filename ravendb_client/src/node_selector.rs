6ce8e640fa96073bfc60f6e3a13db592897b4706
damccull <401104+damccull@users.noreply.github.com>
2022-10-30 17:09:50 -0400

    Add comments detailing the requirements of the NodeSelector

--- ravendb_client/src/node_selector.rs
+++ ravendb_client/src/node_selector.rs
@@ -1,5 +1,17 @@
 use crate::{server_node::ServerNode, topology::Topology};
 
+///! Requirements for the NodeSelector
+/// 1. Maintain the following state:
+///    a. Current topology
+///    b. Number of failures per node
+///    c. Speed of each node (used to determine fastest)
+///    d. Fastest node
+///    e. Default node to try if all nodes in the server have faults
+/// 2. Provide a speed test capability for all nodes
+/// 3. Return the fastest node
+/// 4. Return a specific node
+/// 5. Return a "preferred" node
+/// 6. Return the topology if requested
 #[derive(Debug)]
 pub struct NodeSelector {
     update_fastest_node_timer: bool,
