d3fe252bd7d92189106738cbd56a6ae278e19b23
damccull <401104+damccull@users.noreply.github.com>
2022-10-30 23:30:08 -0400

    Test that tokio spawn can  await a pre-created future. Spoiler alert: It can.

--- ravendb_client/examples/sandbox.rs
+++ ravendb_client/examples/sandbox.rs
@@ -2,6 +2,7 @@ use ravendb_client::{
     raven_command::{RavenCommand, RavenCommandVariant},
     ravendb_error::RavenDbError,
 };
+use reqwest::{Client, Response};
 use url::Url;
 
 #[tokio::main]
@@ -12,7 +13,14 @@ async fn main() -> anyhow::Result<()> {
     };
     let request = raven_command.get_http_request()?;
 
+    let client = Client::new();
+    let f = client.execute(request);
+    let r:Result<Response,reqwest::Error> = tokio::spawn(async move {
+        f.await
+    }).await.unwrap();
+    dbg!(r);
+
     let e: Result<(), RavenDbError> = Err(RavenDbError::DatabaseDoesNotExist("MyDb".to_string()));
-    dbg!(request, e);
+    //dbg!(request, e);
     Ok(())
 }
