61572fa9f53935b261d3ba9337205d8127faef47
damccull <401104+damccull@users.noreply.github.com>
2022-10-30 23:29:34 -0400

    Fix url

--- ravendb_client/examples/sandbox.rs
+++ ravendb_client/examples/sandbox.rs
@@ -7,7 +7,7 @@ use url::Url;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let raven_command = RavenCommand {
-        base_server_url: Url::parse("https://free.a.damccull.ravendb.cloud")?,
+        base_server_url: Url::parse("https://a.free.damccull.ravendb.cloud/")?,
         command: RavenCommandVariant::GetClusterTopology,
     };
     let request = raven_command.get_http_request()?;
