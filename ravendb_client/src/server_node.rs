d1a82fde7ac4e6ddbffb14481c32b91881832baa
damccull <401104+damccull@users.noreply.github.com>
2022-12-20 22:03:04 -0500

    Make properties public and remove accessor methods

--- ravendb_client/src/server_node.rs
+++ ravendb_client/src/server_node.rs
@@ -4,10 +4,10 @@ use crate::cluster_topology::ClusterTopology;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct ServerNode {
-    url: Url,
-    database: String,
-    cluster_tag: String,
-    server_role: ServerRole,
+    pub url: Url,
+    pub database: String,
+    pub cluster_tag: String,
+    pub server_role: ServerRole,
 }
 impl ServerNode {
     pub fn new(url: Url, database: String) -> Self {
@@ -17,22 +17,6 @@ impl ServerNode {
             cluster_tag: String::default(),
             server_role: ServerRole::default(),
         }
-    }
-
-    pub fn set_cluster_tag(&mut self, tag: String) {
-        self.cluster_tag = tag;
-    }
-
-    pub fn get_cluster_tag(&self) -> &str {
-        self.cluster_tag.as_str()
-    }
-
-    pub fn set_server_role(&mut self, role: ServerRole) {
-        self.server_role = role;
-    }
-
-    pub fn get_server_role(&self) -> ServerRole {
-        self.server_role
     }
 
     pub fn supports_atomic_cluster_writes() {
